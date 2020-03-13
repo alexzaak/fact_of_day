3be27ecec9637c6624e6dd7897d144b698978a22
Alexander Zaak <alexander.zaak@otto.de>
2020-03-13 22:21:58 +0100

    refactor: move to root folder

---
+++ android/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/azaak/Library/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/azaak/workspace/fact_of_day/fact_of_day/android"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/azaak/Library/Flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/azaak/Library/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/azaak/workspace/fact_of_day/fact_of_day"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/azaak/Library/Flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.1.0"
+export "FLUTTER_BUILD_NUMBER=12"
