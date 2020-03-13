d79104de26f36d413a800a6303eb96c771802984
Alexander Zaak <alexander.zaak@otto.de>
2020-03-13 23:47:39 +0100

    ops: add release files

---
+++ release/decrypt_keys.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+ENCRYPTION_KEY="$1"
+
+if [[ ! -z "${ENCRYPTION_KEY}" ]]
+then
+    openssl enc -d -aes-256-cbc -md sha256 -in android/release.aes -out android/keyStore.jks -k "${ENCRYPTION_KEY}"
+    openssl enc -d -aes-256-cbc -md sha256 -in android/properties.aes -out android/key.properties -k "${ENCRYPTION_KEY}"
+else
+    echo "ENCRYPTION_KEY is missing"
+fi
