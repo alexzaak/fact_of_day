68723570d25ba65611a02836b2668fec7699ca9e
Alexander Zaak <alexander.zaak@otto.de>
2020-03-14 18:51:13 +0100

    ops: add rpipeline

--- release/decrypt_keys.sh
+++ release/decrypt_keys.sh
@@ -7,6 +7,7 @@ if [[ ! -z "${ENCRYPTION_KEY}" ]]
 then
     openssl enc -d -aes-256-cbc -md sha256 -in android/release.aes -out android/keyStore.jks -k "${ENCRYPTION_KEY}"
     openssl enc -d -aes-256-cbc -md sha256 -in android/properties.aes -out android/key.properties -k "${ENCRYPTION_KEY}"
+    openssl enc -d -aes-256-cbc -md sha256 -in android/service.aes -out android/service_account.json -k "${ENCRYPTION_KEY}"
 else
     echo "ENCRYPTION_KEY is missing"
 fi
--- release/encrypt_keys.sh
+++ release/encrypt_keys.sh
@@ -7,6 +7,7 @@ if [[ ! -z "${ENCRYPTION_KEY}" ]]
 then
     openssl enc -aes-256-cbc -md sha256 -in android/keyStore.jks -out android/release.aes -k "${ENCRYPTION_KEY}"
     openssl enc -aes-256-cbc -md sha256 -in android/key.properties -out android/properties.aes -k "${ENCRYPTION_KEY}"
+    openssl enc -aes-256-cbc -md sha256 -in android/service_account.json -out android/service.aes -k "${ENCRYPTION_KEY}"
 else
     echo "ENCRYPTION_KEY is missing"
 fi
